b133d50519430b187acadc4ca24ada5e590a74a1
SimonB87 <buryans@gmail.com>
2020-09-22 18:21:06 +0200

    added JS main logic

--- nameDaysApi/app.js
+++ nameDaysApi/app.js
@@ -9,13 +9,40 @@ fetch('https://api.abalin.net/today?country=cz')
   .then((resp) => resp.json())
   .then(displayName);
 
-const getResultOfDatetimePlace = (data) => {
+const highlight = () => {
+  const highlightsArray = document.querySelectorAll(".highlight");
+  const numberOfHighlights = highlightsArray.length;
+  for(var i = 0; i < numberOfHighlights; i++) {
+    const chooseElem = highlightsArray[i]
+    chooseElem.classList.add("hasData");
+  }
+}
+
+const getResultOfDatetimePlace = (json) => {
   const day = document.getElementById("dateDay").value;
   const month = document.getElementById("dateMonth").value;
+  const country = document.getElementById("country").value;
   const dateTimeString = `${day}. ${month}.`;
-  document.getElementById("selectedDate").innerText = dateTimeString;
 
-  document.getElementById("selectedCountry").innerText = document.getElementById("country").value;
+  document.getElementById("selectedDate").innerText = dateTimeString;
+  document.getElementById("selectedCountry").innerText = country;
+
+  const displayRequestName = (json) => {
+    const nameElm = document.querySelector('#resultName');
+    nameElm.textContent = json.data.namedays[country];
+  };
+
+  const fetchString = `https://api.abalin.net/namedays?country=${country}&month=${month}&day=${day}`;
+
+  highlight();
+
+  fetch(fetchString)
+  .then((resp) => resp.json())
+  .then(displayRequestName)
 }
+
+document.getElementById("actionTrigger").addEventListener("click", function(){
+  getResultOfDatetimePlace();
+}); 
 
 //https://api.abalin.net/namedays?country=cz&month=2&day=13
