fb16925328aeea2402f8688a3a1c098aa9a3746a
SimonB87 <buryans@gmail.com>
2020-09-22 10:27:36 +0200

    place choosen values into GUI

--- nameDaysApi/app.js
+++ nameDaysApi/app.js
@@ -9,4 +9,13 @@ fetch('https://api.abalin.net/today?country=cz')
   .then((resp) => resp.json())
   .then(displayName);
 
+const getResultOfDatetimePlace = (data) => {
+  const day = document.getElementById("dateDay").value;
+  const month = document.getElementById("dateMonth").value;
+  const dateTimeString = `${day}. ${month}.`;
+  document.getElementById("selectedDate").innerText = dateTimeString;
+
+  document.getElementById("selectedCountry").innerText = document.getElementById("country").value;
+}
+
 //https://api.abalin.net/namedays?country=cz&month=2&day=13
